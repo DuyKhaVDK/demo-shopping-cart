1e4886ae62ab62b99d24ce1de42314e1fc31344c
Kha Võ <duykha392001@gmail.com>
2021-11-25 22:37:39 +0700

    Updates

---
+++ precache-manifest.9451ffe4cb26b26c3fe7ad5f52222bc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a206ec6eecba6e226f9070eec12705dc",
+    "url": "/demo-shopping-cart/index.html"
+  },
+  {
+    "revision": "24908a51d16ce5a1fe85",
+    "url": "/demo-shopping-cart/static/css/main.c9585295.chunk.css"
+  },
+  {
+    "revision": "42aa6bfa0018e2378d70",
+    "url": "/demo-shopping-cart/static/js/2.c1589643.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-shopping-cart/static/js/2.c1589643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24908a51d16ce5a1fe85",
+    "url": "/demo-shopping-cart/static/js/main.7dc12aa6.chunk.js"
+  },
+  {
+    "revision": "13a7c9e3f415c945c6b8",
+    "url": "/demo-shopping-cart/static/js/runtime-main.9714c080.js"
+  }
+]);
